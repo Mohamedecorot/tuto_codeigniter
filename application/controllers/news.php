9e777e6936d63e991bc6109494a78e32a9f0f00e
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-21 14:54:02 +0100

    view: premiere page et recuperation de variable depuis le controlleur

---
+++ application/controllers/news.php
@@ -0,0 +1,24 @@
+<?php
+class News extends CI_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index()
+	{
+		$this->accueil();
+	}
+
+	public function accueil()
+	{
+		$data = array();
+		$data['pseudo'] = 'Arthur';
+		$data['email'] = 'email@ndd.fr';
+		$data['en_ligne'] = true;
+
+		//	Maintenant, les variables sont disponibles dans la vue
+		$this->load->view('vue', $data);
+	}
+}
