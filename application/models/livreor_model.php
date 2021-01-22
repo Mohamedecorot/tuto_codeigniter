663d5be71658b0663c75832b64d18b457c6af84e
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-22 14:57:07 +0100

    Livre_dor : ajout ajouter_commentaire

--- application/models/livreor_model.php
+++ application/models/livreor_model.php
@@ -4,15 +4,23 @@ class Livreor_model extends CI_Model
 {
 	private $table = 'livreor_commentaires';
 
-	public function ajouter_commentaire($pseudo, $message)
-	{
+    public function ajouter_commentaire($pseudo, $message)
+    {
+        if(!is_string($pseudo) OR !is_string($message) OR empty($pseudo) OR empty($message))
+        {
+            return false;
+        }
 
-	}
+        return $this->db->set(array('pseudo' => $pseudo,
+                        'message' => $message))
+                ->set('date', 'NOW()', false)
+                ->insert($this->table);
+    }
 
-	public function count()
-	{
-
-	}
+    public function count()
+    {
+        return $this->db->count_all($this->table);
+    }
 
 	public function get_commentaires($nb, $debut = 0)
 	{
