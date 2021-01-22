842dcc211b98d3e3f43cdf4b6267f3827c167d42
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-01-22 13:57:21 +0100

    MODEL: ajout du squelette du model et du controller pour le model

---
+++ application/models/news_model.php
@@ -0,0 +1,50 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class News_model extends CI_Model
+{
+	protected $table = 'news';
+
+	/**
+	 *	Ajoute une news
+	 */
+	public function ajouter_news()
+	{
+
+	}
+
+	/**
+	 *	Édite une news déjà existante
+	 */
+	public function editer_news()
+	{
+
+	}
+
+	/**
+	 *	Supprime une news
+	 */
+	public function supprimer_news()
+	{
+
+	}
+
+	/**
+	 *	Retourne le nombre de news
+	 */
+	public function count()
+	{
+
+	}
+
+	/**
+	 *	Retourne une liste de news
+	 */
+	public function liste_news()
+	{
+
+	}
+}
+
+
+/* End of file news_model.php */
+/* Location: ./application/models/news_model.php */
